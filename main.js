339c1221d0c0099a206c83966e1866c421368162
devel6236 <63672044+devel6236@users.noreply.github.com>
2020-11-19 14:21:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -155,7 +155,7 @@ class Tado extends utils.Adapter {
 
 							case ('typeSkillBasedApp'):
 								if (set_mode == 'NO_OVERLAY') { break; }
-								if (set_mode == 'null') {
+								if (set_mode == null) {
 									this.log.info('Overlay cleared for room : ' + deviceId[4] + ' in home : ' + deviceId[2]);
 									await this.clearZoneOverlay(deviceId[2],deviceId[4]);
 								}else{
