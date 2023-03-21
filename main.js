f394151bcd90260349ebb2575d5c3437c0ce4eaa
devel6236 <63672044+devel6236@users.noreply.github.com>
2023-03-21 18:45:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -718,8 +718,12 @@ class Tado extends utils.Adapter {
 					break;
 					
 				case ('isBalanceAcEligible'):
-				this.create_state(HomeId + '._info. ' + i, i, JSON.stringify(this.Home_data[i]));
-				break;
+					this.create_state(HomeId + '._info. ' + i, i, JSON.stringify(this.Home_data[i]));
+					break;
+					
+				case ('isEnergyIqEligible'):
+					this.create_state(HomeId + '._info. ' + i, i, JSON.stringify(this.Home_data[i]));
+					break;
 
 				default:
 					this.log.warn('Send this info to developer !!! { Unhandable information found in DoHome : ' + JSON.stringify(i) + ' with value : ' + JSON.stringify(this.Home_data[i]));
